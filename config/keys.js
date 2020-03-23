52f1b7812832f3e8a640ca6e62e3705f8a0f6f80
Sierra <Bell>
2020-03-23 12:29:38 -0400

    testing

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+mongoURI:'mongodb+srv://sierrabell:4dventureAwai7s@cluster0-shpgk.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+//Item Model
+const Item = require('../../models/Item');
+
+//@route GET api/items
+//@desc Get All Items
+//@access Public
+
+router.get('/', (req, res) => {
+  Item.find()
+  .sort({date: -1})
+  .then(items => res.json(items))
+});
+
+
+//@route POST  api/items
+//@desc Create A Item
+//@access Public
+
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+});
+
+//@route DELETE  api/items/:Id
+//@desc Delete A Itwm
+//@access Public
+
+router.delete('/:id', (req, res) => {
+Item.findById(req.params.id)
+.then(item => item.remove().then(() => res.json({sucess: true})))
+.catch(err => res.status(404).json({sucess: false}));
+});
+module.exports = router;
