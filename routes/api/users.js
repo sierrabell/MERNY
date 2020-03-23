7e25bace61ca253c28c7b3b4fed37cb50df7839e
Sierra <Bell>
2020-03-23 19:12:07 -0400

    first

---
+++ routes/api/users.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//USer Model
+const User = require('../../models/User');
+
+//@route GET api/users
+//@desc  Register new user
+//@access Public
+
+router.post('/', (req, res) => {
+  const { name, email, password } = req.body;
+
+  //simple validation
+  if(!name || !email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields'});
+  }
+
+  //check for existing user
+  User.findOne({ email })
+  .then(user => {
+    if(user) return res.status(400).json({ msg: 'user already exists'});
+
+    const newUser = new User({
+      name,
+      email,
+      password
+    });
+
+    // create salt and hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save()
+        .then(user => {
+          res.json({
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email
+            }
+          })
+        })
+      })
+    })
+  })
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
  const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const items = require('./routes/api/items');
@@ -10,19 +9,24 @@ const app = express();
 
 //Bodyparser Middleware
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
 
 //connect to Mongo
 
-mongoose.connect(db)
+mongoose
+.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}) //adding new mongo url parser
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
 //use routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 //serve static assests if in production
 if(process.env.NODE_ENV === 'production') {
@@ -30,7 +34,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 const port = process.env.PORT || 5000;
